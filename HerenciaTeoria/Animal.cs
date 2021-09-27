77428f826180ab883e11c081903ee2aa84636706
Geovany Alvarez <smis063721@ugb.edu.sv>
2021-09-27 15:50:18 -0600

    Add project files.

---
+++ HerenciaTeoria/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaTeoria
+{
+    //clase padre o base
+    class Animal
+    {
+        //propiedades
+
+        public int animalAge { get; set; }
+        public Boolean animalFur { get; set; }
+
+        //metodos
+
+        public string getInfo()
+        {
+            var fur = "";
+            if (animalFur == true)
+            {
+                fur = "Tiene pelo ";
+            }
+            else
+            {
+                fur = "No tiene pelo ";
+            }
+            return "El animal tiene " + animalAge + " años, y " + fur;
+        }
+        public string walk()
+        {
+            return "El animal esta caminando ";
+        }
+
+        //overload del metodo walk
+        public string walk(string name, string owner)
+        {
+            return "El" + name + "Esta caminando con " + owner; 
+        }
+        public virtual string run()
+        {
+            return "El animal esta corriendo ";
+        }
+    }
+}
---
+++ HerenciaTeoria/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaTeoria
+{
+    //clase hijo o derivado
+    //definimos la relacion 
+    class Cat:Animal
+    {
+        //propiedades
+        public int lives { get; set; }
+
+        //metodos
+        public string meow()
+        {
+            return "meooowww";
+        }
+
+        //metodo sobreescrito
+        public override string run()
+        {
+            return base.run() + "El gato esta corriendo... ";
+        }
+    }
+}
